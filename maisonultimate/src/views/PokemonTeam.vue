ae3128a679761628d03f8daea15f3141fec1c58f
Yuri <jdsanchez19@esei.uvigo.es>
2023-08-10 19:36:01 +0200

    Added HTML semantics and CSS to Team Cards

--- maisonultimate/src/views/PokemonTeam.vue
+++ maisonultimate/src/views/PokemonTeam.vue
@@ -1,18 +1,15 @@
 <template>
-  <h1>Pokemon Team Planner</h1>
 
-  <div class="pkmn-cards-container">
-    <div class="pkmn-card" v-for="member in store.team" :key="member">
-      <p class="pkmn-card-info" >{{ member.species }}</p>
-      <button @click="store.removeFromTeam(member.species)">Remove</button>
+  <div class="Pokemon-Cards-Container">
+    <div class="Pokemon-Card" v-for="member in store.team" :key="member">
+      <img :src="member.sprite" alt="" width="80" height="80">
     </div>
   </div>
 
-  <div id="Pkmn">
-    <h2>Search for a result:</h2>
-    <YuriSeach :items="allPkmn" placeholderText="Elige un Pokemon..." attrToShow="species" :minLength="1"
-      @onItemSelected="(event) => updateSelection(event)" />
-  </div>
+  <h2>Search for a result:</h2>
+  <YuriSeach :items="allPkmn" placeholderText="Elige un Pokemon..." attrToShow="species" :minLength="1"
+    @onItemSelected="(event) => updateSelection(event)" />
+
 </template>
 
 
@@ -32,10 +29,37 @@ const updateSelection = (item) => {
   store.addToTeam(item)
 }
 
-/* store.addToTeam(allPkmn[0]) */
-/* store.removeFromTeam(allPkmn[0].species) */
 </script>
 
 
 
-<style scoped></style>
+<style scoped>
+.Pokemon-Cards-Container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  gap: 30px;
+  /* margin-top: 20px; */
+}
+
+.Pokemon-Card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 120px;
+  background-color: rgb(220, 220, 220);
+  border: 1px solid transparent;
+  border-radius: 0px 0px 10px 10px;
+}
+
+.Pokemon-Card img {
+  margin-bottom: 10px;
+}
+
+.Pokemon-Card:hover{
+  background-color:#97C5BD;
+  cursor:pointer;
+}
+</style>
